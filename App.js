72aa9c75956c39e8cfc9fc9f4675ba14c7cf2106
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-06-14 06:15:48 -0300

    img e cor de fundo

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <Image 
+     source={require('./src/crono.png')}
+     />
+     
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
 });
